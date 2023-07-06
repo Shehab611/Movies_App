afdf068cb9592b5f1ecf8656616fcdc69a7f48bd
Shehab Ehab <77563526+Shehab611@users.noreply.github.com>
2023-07-06 19:30:41 +0300

    Update constants.dart

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,11 +1,11 @@
 class WeatherAppConstants{
   static String baseurl='https://api.openweathermap.org/data/2.5';
-  static String apiKey='f476b6294c20e8166b3639fbcc2f8863';
+  static String apiKey='';
 
 }
 class MoviesAppConstants{
   static String baseurl='https://api.themoviedb.org/3';
-  static String apiKey='f476b6294c20e8166b3639fbcc2f8863';
+  static String apiKey='';
   static String baseImageUrl='https://image.tmdb.org/t/p/w500';
   static String getNowPlayingPath='$baseurl/movie/now_playing?api_key=$apiKey';
   static String getPopularMoviesPath='$baseurl/movie/popular?api_key=$apiKey';
